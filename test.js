b5e046f479adb39019935f4794a6bfabc7eb0800
Aliaksei Sapach <dreamsonic@tut.by>
2018-06-01 22:19:37 +0300

    added a test for styles

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
-QUnit.test('should set innerHTML', function (assert) {
-    document.querySelector('#qunit-fixture').innerHTML = 'one';
+QUnit.test("should set innerHTML", function (assert) {
+    document.querySelector("#qunit-fixture").innerHTML = "one";
     assert.expect(0);
 });
 
-QUnit.test('should verify that innerHTML is unchaged', function (assert) {
-    assert.equal(document.querySelector('#qunit-fixture').innerHTML, '');
+QUnit.test("should verify that innerHTML is unchaged", function (assert) {
+    assert.equal(document.querySelector("#qunit-fixture").innerHTML, "");
+});
+
+QUnit.test("should verify the styles", function (assert) {
+    var coords = document.querySelector("#qunit-fixture").getBoundingClientRect();
+    assert.propEqual(coords, {
+        bottom: -9000,
+        height: 1000,
+        left: -10000,
+        right: -9000,
+        top: -10000,
+        width: 1000
+    });
 });
